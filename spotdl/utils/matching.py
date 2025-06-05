4bbc1055d1380457d76dbfbb8a5cc8b687d1598a
CJuette <16166683+CJuette@users.noreply.github.com>
2025-06-04 19:23:44 -0700

    Revert "remove duplicates"
    This reverts commit fa03ba08cd0b7204d37d7af95255c6df6c447aba.

--- spotdl/utils/matching.py
+++ spotdl/utils/matching.py
@@ -50,10 +50,13 @@ FORBIDDEN_WORDS = [
     "acoustic",
     "8daudio",
     "concert",
+    "live",
     "acapella",
     "slowed",
     "instrumental",
+    "remix",
     "cover",
+    "reverb",
 ]
 
 
