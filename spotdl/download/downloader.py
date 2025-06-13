d5fe8bff4f7f6e35652cc743683c28b9cb4e8fe9
Chritzel <16166683+Chritzel@users.noreply.github.com>
2025-06-12 23:16:05 -0700

    Add logging for extended version search results with artist and name details

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -633,6 +633,8 @@ class Downloader:
                                 logger.debug("User chose not to use extended version for %s", song.display_name)
                         else:
                             # fallback: always use
+                            result_display_name = ", ".join(best_result.artists if best_result.artists else best_result.author) + " - " + best_result.name
+                            logger.warning("Found extended version for '%s': '%s' (%s)", song.display_name, result_display_name, best_result.url)
                             return best_result
                 else:
                     return best_result
